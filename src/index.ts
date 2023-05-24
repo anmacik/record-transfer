faf631bc441f295d7c83639de8fc029b0429aadd
dmitry-crmpbx <dmitry@salesautomators.com>
2023-05-24 17:38:22 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,53 @@
-const addition = (a: number, b: number) => {
-  return a + b;
-};
+import dotenv from 'dotenv';
+import * as path from 'path';
+import * as fs from 'fs';
+import express, { Express, Request, Response } from 'express';
+import https from 'https';
+import bodyParser from 'body-parser';
+import request from 'request';
 
-const number1 = 5;
-const number2 = 10;
-const result = addition(number1, number2);
+import { GoogleDriveService } from './googleDriveService';
+const app: Express = express();
+app.use(bodyParser.json());
+dotenv.config();
 
-console.log('The result is %d', result);
+const port = process.env.PORT;
+const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || '';
+const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
+const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
+const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
+
+const googleDriveService =  new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+var count = 0;
+app.post('/', async (req: Request, res: Response) => {
+  let flName=count;
+  count++;
+  console.log('new file', req.body.url)
+  dlFile(req.body.url, flName).then(async ()=>{
+  const finalPath = path.resolve(__dirname, '../public/'+flName+'.mp3');
+  const folderName = 'records';
+  let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
+    console.error(error);
+    return null;
+  });
+
+  if (!folder) {
+    folder = await googleDriveService.createFolder(folderName);
+  }
+  var respond = await googleDriveService.saveFile(`${req.body.url}`, finalPath, '[*/*]', folder.id).catch((error) => {
+    console.error(error);
+  });
+  fs.unlinkSync(finalPath);
+  res.send({url:'https://drive.google.com/file/d/'+respond.data.id});
+  })
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+const dlFile = async (url:string, fileName) =>{
+  request
+  .get(url)
+  .on('error', err=>{console.log(err)})
+  .pipe(fs.createWriteStream('./public/'+fileName+'.mp3'))
+}
